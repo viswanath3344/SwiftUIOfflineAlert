2efa7ec84fc02aa3c9cd6f8f9d241e75f1aa0494
Viswanath Reddy Ponthota (Contractor) <viswanathreddy.ponthota@cognizant.com>
2024-08-07 11:36:07 +0530

    Initial Commit

---
+++ SwiftUIOfflineAlert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIOfflineAlert
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 07/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIOfflineAlert/SwiftUIOfflineAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOfflineAlertApp.swift
+//  SwiftUIOfflineAlert
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOfflineAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
